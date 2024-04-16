44ec7e63ef90b8943e621efd43438b84cb5797c6
Patrik Sokolowski <wunderward@gmail.com>
2024-04-16 18:41:02 -0400

    Checkbox and Update functionality for ToDo items

--- app.py
+++ app.py
@@ -38,6 +38,20 @@ def create_todo():
             abort(400)
         else:            
             return jsonify(body)
+        
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    todo = Todo.query.get(todo_id)
+    todo.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index'))
 
 @app.route('/')
 def index():
